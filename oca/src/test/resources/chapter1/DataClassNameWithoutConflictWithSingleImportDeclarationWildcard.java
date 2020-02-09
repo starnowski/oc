0e7e1ae203345425643c52ce10c63c84446e7570
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-02-09 13:43:18 +0100

    Added more detail description

--- oca/src/test/resources/chapter1/DataClassNameWithoutConflictWithSingleImportDeclarationWildcard.java
+++ oca/src/test/resources/chapter1/DataClassNameWithoutConflictWithSingleImportDeclarationWildcard.java
@@ -3,7 +3,8 @@ import java.sql.Date;
 import java.util.*;
 
 /**
- * Compilation should be successful.<br/>
+ * Compilation should be successful even when the import declaration for package "java.util" contains a wildcard.<br/>
+ * Fortunately the import declaration for type "java.sql.Date" is unequivocal.<br/>
  *  javac DataClassNameWithoutConflictWithSingleImportDeclarationWildcard.java
 */
 public class DataClassNameWithoutConflictWithSingleImportDeclarationWildcard {
