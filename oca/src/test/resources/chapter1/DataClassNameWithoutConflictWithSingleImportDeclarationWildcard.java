a477932f2db19b0707230d88d1692e1b408bb3d0
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-02-09 13:31:55 +0100

    Added test

---
+++ oca/src/test/resources/chapter1/DataClassNameWithoutConflictWithSingleImportDeclarationWildcard.java
@@ -0,0 +1,12 @@
+
+import java.sql.Date;
+import java.util.*;
+
+/**
+ * Compilation should be successful.<br/>
+ *  javac DataClassNameWithoutConflictWithSingleImportDeclarationWildcard.java
+*/
+public class DataClassNameWithoutConflictWithSingleImportDeclarationWildcard {
+	
+	private Date date;
+}
