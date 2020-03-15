29717673036eaa2c09fd41ffc1d265c0b87b4451
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-03-15 20:08:02 +0100

    Changed number of invalid code line.

--- oca/src/test/resources/chapter1/st/innerclassimports/ClassWithImportInnerClass.java
+++ oca/src/test/resources/chapter1/st/innerclassimports/ClassWithImportInnerClass.java
@@ -4,7 +4,7 @@ import st.innerclassimports.subpackage.ClassWithManyInnerClassess.InnerClass;
 /**
  * <br/>javac "./st/innerclassimports/ClassWithImportInnerClass.java" "./st/innerclassimports/subpackage/ClassWithManyInnerClassess.java" 
  * <p/>
- .\st\innerclassimports\ClassWithImportInnerClass.java:9: error: package ClassWithManyInnerClassess does not exist
+ .\st\innerclassimports\ClassWithImportInnerClass.java:14: error: package ClassWithManyInnerClassess does not exist
         public ClassWithManyInnerClassess.InnerClass obj;
                                          ^
 1 error
