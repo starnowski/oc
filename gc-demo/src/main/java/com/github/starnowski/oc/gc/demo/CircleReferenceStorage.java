2dad2883d72c7ea8ceb3fad23ccd681441bb84a0
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-06-11 20:43:11 +0200

    Changed method variables into instance variables.

--- gc-demo/src/main/java/com/github/starnowski/oc/gc/demo/CircleReferenceStorage.java
+++ gc-demo/src/main/java/com/github/starnowski/oc/gc/demo/CircleReferenceStorage.java
@@ -2,9 +2,12 @@ package com.github.starnowski.oc.gc.demo;
 
 public class CircleReferenceStorage {
 
+    private SimpleObjectReference ref1;
+    private SimpleObjectReference ref2;
+
     public CircleReferenceStorage() {
-        SimpleObjectReference ref1 = new SimpleObjectReference();
-        SimpleObjectReference ref2 = new SimpleObjectReference();
+        ref1 = new SimpleObjectReference();
+        ref2 = new SimpleObjectReference();
         ref1.setReference(ref2);
         ref2.setReference(ref1);
     }
