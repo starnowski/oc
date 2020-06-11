73af067fcae2969125efff9119423fc1d2a3c8c1
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2020-06-11 19:46:55 +0200

    Added Main class for gc demo.

---
+++ gc-demo/src/main/java/com/github/starnowski/oc/gc/demo/MainWithMBeanServer.java
@@ -0,0 +1,22 @@
+package com.github.starnowski.oc.gc.demo;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+
+public class MainWithMBeanServer {
+
+    public static void main(String[] args)
+            throws Exception {
+
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("com.example:type=Hello");
+        Hello mbean = new Hello();
+        mbs.registerMBean(mbean, name);
+
+        //
+
+        System.out.println("Waiting forever...");
+        Thread.sleep(Long.MAX_VALUE);
+    }
+}
